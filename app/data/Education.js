81db6cc054db18ea2a055b5875914651c924d4f4
Samarth Jain <samarthj0612@gmail.com>
2024-07-24 22:06:28 +0530

    Added data for experience and education

---
+++ app/data/Education.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    education: "BACHELORS OF TECHNOLOGY IN INFORMATION TECHNOLOGY",
+    university: "RAJIV GANDHI PROUDYOGIKI VISHWAVIDYALAYA [RGPV]",
+    start: "2020",
+    end: "2024",
+    description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+  },
+  {
+    education: "12TH(PCM)",
+    university: "GB PANT HIGHER SECONDARY SCHOOL",
+    start: "2019",
+    end: "2020",
+    description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+  },
+  {
+    education: "10TH",
+    university: "GB PANT HIGHER SECONDARY SCHOOL",
+    start: "2017",
+    end: "2018",
+    description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+  },
+]
---
+++ app/data/Experience.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    role: "Software Developer",
+    organization: "Brandsmashers Technologies Pvt. Ltd.",
+    joining: "Feb 2024",
+    description: `Lead and mentor a team of developers, fostering a collaborative environment that encourages innovation and excellence.Oversee end-to-end project execution, from requirement gathering and architecture design to deployment and maintenance.`
+  },
+  {
+    role: "JUNIOR SOFTWARE DEVELOPER",
+    organization: "HPS Lab Designs Pvt. Ltd.",
+    joining: "May 2023",
+    leaving: "Nov 2023",
+    description: `It has been a fantastic learning journey, where I have been exposed to a diverse range of technologies and methodologies. My contributions ranged from implementing new features to resolving bugs and ensuring the overall stability of the application`
+  },
+]
